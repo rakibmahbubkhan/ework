2557e646065b74d37427386483c479c0d4dd4277
rakibmahbubkhan <52800057+rakibmahbubkhan@users.noreply.github.com>
2020-08-10 20:44:06 +0600

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,13 +1,14 @@
 <?php
-$dsn = 'mysql:dbhost=ec2-35-173-94-156.compute-1.amazonaws.com;dbname=dmd16vhafqbm6';
-$dbuser = 'ctxwlasfztqsmg@ework';
-$password = 'b7413c47149b7038ce56cc49e464c09f8044ab76afc5459a677ddaf201b3578e';
-$option = array(
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-);
 try{
-	$con = new PDO($dsn,$dbuser,$password,$option);
-	$con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+		"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		$db["host"],
+		$db["port"],
+		$db["user"],
+		$db["pass"],
+		ltrim($db["path"], "/")
+	));	
 }
 catch(PDOEXCEPTION $e){
 		echo "Database Connecton Failed!!";
